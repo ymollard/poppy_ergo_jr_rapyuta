d19a268210f3d4c4b4e6747db8d1cb82acac27f8
Yoan Mollard <ymollard@users.noreply.github.com>
2021-02-12 19:12:38 +0100

    Simple move example

---
+++ poppy_ergo_jr_move_example/src/move.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import rospy
+from moveit_commander.move_group import MoveGroupCommander
+rospy.init_node('move_example')
+
+rospy.loginfo("Connecting to move group...")
+commander = MoveGroupCommander("arm_and_finger")
+rospy.loginfo("Connected!")
+
+rate = rospy.Rate(1)
+while not rospy.is_shutdown():
+    rospy.loginfo("Going to zero...")
+    commander.set_joint_value_target([0, 0, 0, 0, 0, 0])
+    commander.go()
+    rospy.loginfo("Going to a pose target...")
+    commander.set_pose_target([0, 0, 0.25] + [1, 0, 0, 0])
+    commander.go()
+    rate.sleep(1)
